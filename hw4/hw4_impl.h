7ba032078ef2fd01ede6c2625ed69a3ce018dc63
Laura <laura.nagamine@utec.edu.pe>
2024-11-23 17:14:15 -0500

    hw4 primera funcion algoritmo intuitivo finished

--- hw4/hw4_impl.h
+++ hw4/hw4_impl.h
@@ -7,12 +7,28 @@
 #include "hw4.h"
 
 std::vector<int> stringMatch_naive(std::string const& text,
-                                   std::string const& pattern)
-{
+                                   std::string const& pattern){
   // Implemente aqui el método mas ingenuo para resolver el problema de string
   // matching.
+  // metodo ingenuo como visto en clase (algoritmo intuitivo)
 
   std::vector<int> ret;
+
+  int n = text.size();
+  int m = pattern.size();
+
+
+  for (int j=0;j<= n-m;++j) {
+      int i = 0;
+
+      while (i < m && text[j + i] == pattern[i]) {
+          ++i;
+      }
+
+      if (i == m) {
+          ret.push_back(j);
+      }
+  }
   return ret;
 }
 
